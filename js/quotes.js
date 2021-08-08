a90e5710f20114ad0e114f4bb45f5cc31b7a99b9
dohui ko <ksder99@likelion.org>
2021-08-08 21:10:29 +0900

    initial commit

---
+++ js/quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+{
+    quote: "It is better to have loved and lost, than never to have loved at all",
+    author: "Alfred, Lord Tennyson",
+},
+{
+    quote: "There is always some madness in love. But there is also always some reason in madness",
+    author: "Friedrich Nietzsche",
+},
+{
+    quote: "Love, free as air at sight of human ties, Spreads his light wings, and in a moment flies.0",
+    author: "Alexander Pope",
+},
+{
+    quote: "Love asks me no questions, and gives me endless support",
+    author: " William Shakespeare",
+}
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote+"\n";
+author.innerText = todaysQuote.author;
