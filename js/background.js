f4354f2ba961856debfacd86778048a434311959
dohui ko <ksder99@likelion.org>
2021-08-08 22:11:14 +0900

    ddd

--- js/background.js
+++ js/background.js
@@ -5,13 +5,11 @@ const IMG_NUMBER = 4;
 
 function paintImage(imgNumber) {
   const image = new Image(); // 이미지를 삽입하는 함수이다
-  image.src = `img/${imgNumber+1}.JPG`; // img폴더안에 있는 파일을 지정한다. imgNumber뒤에 1을 더한 이유는 아래 기술했다.
+  image.src = `img/${imgNumber}.JPG`; // img폴더안에 있는 파일을 지정한다. imgNumber뒤에 1을 더한 이유는 아래 기술했다.
   image.classList.add("bgImage"); // bgImage 클래스를 img 에 추가하여 세부조정 할 수 있게 한다.
   body.appendChild(image);
 }
 //imgNumber뒤에 1을 더한 이유:
-//Math.floor(Math.random() * 3) 가 0,1,2 만 뽑아내기 때문에 1을 더해서 1,2,3 이 되게 하는 것이다(img 파일안에는 1.jpg, 2.jpg, 3.jpg 밖에 없다)
-
 function genRandom() {
   const number = Math.floor(Math.random() * IMG_NUMBER);
   return number;
